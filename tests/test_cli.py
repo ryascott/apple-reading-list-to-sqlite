4c643af414abc79aa76ba28f6d12a35982dd4e61
ryanscott <ryscott@gmail.com>
2023-04-15 13:55:34 -0400

    Add test to make sure full text search table exists when enabling it from cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,6 +3,8 @@ import pathlib
 import pytest
 from typer.testing import CliRunner
 
+from sqlite_utils import Database
+
 from apple_reading_list_to_sqlite.cli import app
 
 runner = CliRunner()
@@ -29,3 +31,14 @@ def test_usage_error_raised():
     assert (
         "Please specify a path to a database file, or use --dump to see the output"
     ) in result.output.strip()
+
+
+def test_fts_search_enabled(tmp_path):
+    db_path = tmp_path / "testfts.db"
+    print(str(db_path))
+    result = runner.invoke(app, [str(db_path), "--enable-fts"])
+    assert result.exit_code == 0
+    assert db_path.exists()
+    assert db_path.stat().st_size > 0
+    db = Database(db_path)
+    assert db["reading_list"].detect_fts() == 'reading_list_fts'
